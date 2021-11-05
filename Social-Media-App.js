a170c6334a51a5c2dffbe7df00e5a01ace89abc7
ZacAndCoder <91081344+ZacAndCoder@users.noreply.github.com>
2021-11-05 13:17:57 -0400

    Make all tabs affect display and add "Dark Mode"
    I initialized the site with only the login page being displayed. I then created a function that displays the main content once the user logs in. I have event listeners that allow the user to go to any clicked tab by changing each element's display. I made the "Dark Mode" switch functional. I prevented posts from being "liked" multiple times and added an icon and username to submitted comments.

--- Social-Media-App.js
+++ Social-Media-App.js
@@ -1,3 +1,14 @@
+document.querySelectorAll("header, #content").forEach(item => {
+  item.style.display = "none";
+});
+
+document.querySelector("#guest-login").addEventListener("click", function() {
+  document.querySelector("#login-page").style.display = "none";
+  document.querySelectorAll("header, #content").forEach(item => {
+    item.style.display = "block";
+  });
+});
+
 //This is the suggested method to initialize tooltips on https://getbootstrap.com/docs/5.0/components/tooltips/ 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
@@ -5,29 +16,66 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 })
 
 document.querySelector("#settings-link").addEventListener("click", function() {
-  document.querySelector("#content").style.display = "none";
+  document.querySelectorAll("#content, #notifications").forEach(item => {
+	item.style.display = "none";
+  });
   document.querySelector("#settings").style.display = "block";
 });
 document.querySelector("#home-link").addEventListener("click", function() {
-  document.querySelector("#settings").style.display = "none";
+  document.querySelectorAll("#settings, #notifications").forEach(item => {
+	item.style.display = "none";
+  });
   document.querySelector("#content").style.display = "block";
+});
+document.querySelector("#notifications-link").addEventListener("click", function() {
+  document.querySelectorAll("#settings, #content").forEach(item => {
+	item.style.display = "none";
+  });
+  document.querySelector("#notifications").style.display = "block";
+});
+
+document.querySelector("#dark-mode-switch").addEventListener("click", function() {
+  document.querySelectorAll("html, body, nav").forEach(item => {
+    item.classList.toggle("dark");
+  });
+  document.querySelectorAll(".nav-item").forEach(item => {
+    item.classList.toggle("btn-dark");
+  });
+  document.querySelectorAll(".card").forEach(item => {
+    item.classList.toggle("bg-dark");
+  }); 
+  document.querySelectorAll("p, .bi-house-fill, .bi-envelope-fill, .bi-gear-fill").forEach(item => {
+    item.classList.toggle("dark-navlink");
+  });
+  document.querySelectorAll("textarea").forEach(item => {
+    item.classList.toggle("dark-comment");
+  });
+  document.querySelectorAll(".accordion-button, .accordion-body, .form-control").forEach(item => {
+    item.classList.toggle("bg-dark");
+    item.classList.toggle("text-white");
+  });
 });
 
 var likedPosts = [];
 
 //Color "like" icons red when clicked. Clone the parent card element and add it to the Liked Posts section.
 document.querySelectorAll(".bi-suit-heart").forEach(item => {
-  //Figure out how to stop post from being liked multiple times
   item.addEventListener("click", function() {
-    if (likedPosts.includes(item.parentNode.parentNode)) {
+    if (likedPosts.includes(item.parentNode.parentNode.parentNode)) {
       return;
     } else {
+      document.querySelector("#no-likes").style.display = "none";
       item.style.color = "red";
       let post = item.parentNode.parentNode.parentNode;
       likedPosts.push(post);
       let clonedPost = post.cloneNode(true); 
+      for (let i = 0; i < clonedPost.childNodes.length; i++) {
+        if (clonedPost.childNodes[i].className == "card-footer") {
+          clonedPost.removeChild(clonedPost.childNodes[i]);
+        }
+      }
       document.querySelector("#liked-posts").appendChild(clonedPost);
-    }
+    } 
   });
 });
 
@@ -54,14 +102,6 @@ document.querySelectorAll(".bi-chat-left").forEach(item => {
     item.style.color = "red";
     if (item == document.querySelector("#usf-comment")) {
       document.querySelector("#usf-comment-box").style.display = "block";
-      /*var commentBox = document.createElement("div");
-      var comment = document.createElement("textarea");
-      var submitComment = document.createElement("button");
-      submitComment.className = "btn-sm btn-primary";
-      submitComment.innerText = "Submit";
-      commentBox.appendChild(comment);
-      commentBox.appendChild(submitComment);
-      item.parentNode.parentNode.appendChild(commentBox); */
     } else if (item == document.querySelector("#aurelius-comment")) {
       document.querySelector("#aurelius-comment-box").style.display = "block";
     } else if (item == document.querySelector("#blackjack-comment")) {
@@ -86,11 +126,14 @@ document.querySelectorAll(".btn-sm").forEach(item => {
   item.addEventListener("click", function() {
     //Maybe prevent submission if nothing has been typed in comment box
     item.parentNode.childNodes[1].disabled = true;
-    /*var username = document.createElement("p");
-    username.innerText = "Guest";
-    item.parentNode.appendChild(username); */
+    var profileIcon = document.createElement("i");
+    profileIcon.className = "bi";
+    profileIcon.classList.add("bi-person-bounding-box");
+    item.parentNode.appendChild(profileIcon);
+    var username = document.createElement("p");
+    username.innerHTML = "@Guest";
+    username.className = "d-inline";
+    item.parentNode.appendChild(username);
     item.style.display = "none";
   });
 });
-
-
